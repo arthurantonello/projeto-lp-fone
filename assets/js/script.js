c09c935e3c5a58f3314709ad77cd0bbb9571b333
Arthur Antonello <arthur.antonello@hotmail.com>
2023-05-17 16:52:56 -0300

    17.05

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,8 @@
-
-//Em testes
-function btnbag(){
-    var action = document.querySelector('#container-button #button-bag')
-    var novaImg = 'url(./img/shopping-cart-contrast.png)'
-    for (var i = 0; i < action.length; i++){
-        var checkAction = action[i];
-        checkAction.addEventListener('mouseover', function(btnbag){
-            this.querySelector('img').src = novaImg
-        })
+function confirmacao(){
+    var input = document.getElementById('input-text')
+    if (input.value == ""){
+        window.modalErro.showModal()
+    }else{
+        window.modalOk.showModal()
     }
 }
-
-btnbag()
